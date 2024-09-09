46da83678dbf870f8e7bdb214c7de50d4a31ee75
ducmanh <phanmanhk57a1@gmail.com>
2024-09-10 05:30:27 +0700

    add install lib

---
+++ install/fastdds_statistics_backend/lib/cmake/fastdds_statistics_backend/fastdds_statistics_backend-config.cmake
@@ -0,0 +1,43 @@
+set(fastdds_statistics_backend_VERSION 1.1.0)
+
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was Config.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+if(MSVC OR MSVC_IDE)
+    set_and_check(fastdds_statistics_backend_BIN_DIR "${PACKAGE_PREFIX_DIR}/bin")
+endif()
+set_and_check(fastdds_statistics_backend_INCLUDE_DIR "${PACKAGE_PREFIX_DIR}/include")
+set_and_check(fastdds_statistics_backend_LIB_DIR "${PACKAGE_PREFIX_DIR}/lib")
+
+if(NOT fastcdr_FOUND)
+    find_package(fastcdr)
+endif()
+
+if(NOT fastrtps_FOUND)
+    find_package(fastrtps)
+endif()
+
+include(${CMAKE_CURRENT_LIST_DIR}/fastdds_statistics_backend-targets.cmake)
